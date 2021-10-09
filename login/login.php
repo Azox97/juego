8d0a462ab64394d6fc06d22a507af41b3a62b41b
SamuelGB1 <samuelgilbarceloiherm.amoros@gmail.com>
2021-10-09 12:33:48 +0200

    Creación login falta realizar las funcionalidades de Olvido de contraseña y Registro y Redirección

---
+++ login/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Login</h1>
+        <form method="post">
+            <div class="camptext">
+                <input type="text" required>
+                <span></span>
+                <label>Usuario</label>
+            </div>
+            <div class="camptext">
+                <input type="password" required>
+                <span></span>
+                <label>Contraseña</label>
+            </div>
+            <div class="pass">¿Olvidaste la Contraseña?</div>
+            <input type="submit" value="Login">
+            <div class="register_link">
+                ¿No eres miembro? <a href="#">Resgistrate</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>

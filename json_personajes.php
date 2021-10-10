eea26f37e60ea67f39ddac2bf63fe5c6ec57c22d
SamuelGB1 <samuelgilbarceloiherm.amoros@gmail.com>
2021-10-10 22:33:37 +0200

    Creación json con los datos de los personajes

---
+++ json_personajes.php
@@ -0,0 +1,100 @@
+<?php
+  $datos_json = '[
+    {
+      "id": 0,
+      "nombre": "96372838P",
+      "altura": 3.2,
+      "peso": 90,
+      "imagen": "",
+      "icono": "",
+      "descripcion": "Monstruo de tres metros y medio",
+      "tipo": "Orco",
+      "fuerza": 300,
+      "inteligencia": 40
+    },
+    {
+      "id": 1,
+      "nombre": "20378965L",
+      "altura": 0.50,
+      "peso": 100,
+      "imagen": "",
+      "icono": "",
+      "descripcion": "Enano de la familia MIN",
+      "tipo": "enano",
+      "dinastia": "MIN",
+      "clan": "Los cabezas huecas" 
+    },
+    {
+      "id": 2,
+      "nombre": "74243726D",
+      "altura": 1.90,
+      "peso": 45,
+      "imagen": "",
+      "icono": "",
+      "descripcion": "Jugador del Call of Duty",
+      "tipo": "Hombre",
+      "color_pelo": "rubio",
+      "color_ojos": "verdes"
+    },
+    {
+      "id": 3,
+      "nombre": "64503736W",
+      "altura": 1.20,
+      "peso": 60,
+      "imagen": "",
+      "icono": "",
+      "descripcion": "Personaje sacado del cuento de Navidad",
+      "tipo": "Elfo",
+      "habitat": "la casa de Papa Noel",
+      "poderes": "Empaquetar regalos"
+    },
+    {
+      "id": 4,
+      "nombre": "63782016E",
+      "altura": 4.5,
+      "peso": 120,
+      "imagen": "",
+      "icono": "",
+      "descripcion": "Orco de la cienaga del lago NESS",
+      "tipo": "Orco",
+      "fuerza": 400,
+      "inteligencia": -1
+    },
+    {
+      "id": 5,
+      "nombre": "6378252T",
+      "altura": 0.90,
+      "peso": 30,
+      "imagen": "",
+      "icono": "",
+      "descripcion": "Mago de cuento",
+      "tipo": "Elfo",
+      "habitat": "La madriguera del señor de los anillos",
+      "poderes": "fabricar pociones mágicas"
+    },
+    {
+      "id": 6,
+      "nombre": "93728602R",
+      "altura": 1.60,
+      "peso": 50,
+      "imagen": "",
+      "icono": "",
+      "descripcion": "Jugador del Minecraft",
+      "tipo": "Hombre",
+      "color_pelo": "castaño",
+      "color_ojos": "marrón"
+    },
+    {
+      "id": 7,
+      "nombre": "13086203W",
+      "altura": 0.90,
+      "peso": 10,
+      "imagen": "",
+      "icono": "",
+      "descripcion": "Enano del Hobbit",
+      "tipo": "Enano",
+      "dinastia": "Thorin",
+      "clan": "Thráin"
+    }
+  ]'
+?>

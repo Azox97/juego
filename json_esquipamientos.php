b989cba350f19015881dfb22a2547999de00959a
Azox97 <73659420+Azox97@users.noreply.github.com>
2021-10-11 17:53:30 +0200

    json_Equipo

---
+++ json_esquipamientos.php
@@ -0,0 +1,58 @@
+<?php
+  $datos_json = '[
+    {
+      "id": 0,
+      "nombre":"Excalibur",
+      "antigüedad":,
+      "dimension_largo":,
+      "dimension_ancho":,
+      "peso":,
+      "imagen":,
+      "icono":,
+      "descripcion":,
+      "corte":,
+      "material":
+    },
+    {
+      "id": 1,
+      "nombre":,
+      "antigüedad":,
+      "dimension_largo":,
+      "dimension_ancho":,
+      "peso":,
+      "imagen":,
+      "icono":,
+      "descripcion":,
+      "alcance":,
+      "perforacion":
+       
+    },
+    {
+      "id": 2,
+      "nombre":,
+      "antigüedad":,
+      "dimension_largo":,
+      "dimension_ancho":,
+      "peso":,
+      "imagen":,
+      "icono":,
+      "descripcion":;
+      "madera":,
+      "rango":
+      
+    },
+    {
+      "id": 3,
+      "nombre":,
+      "antigüedad":,
+      "dimension_largo":,
+      "dimension_ancho":,
+      "peso":,
+      "imagen":,
+      "icono":,
+      "descripcion":,
+      "RompeAemaduras":,
+      "durabilidad":
+    }
+]'
+?>

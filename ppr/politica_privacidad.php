80af7624d474d1ff65a08b9bc7458ae9319247dd
SamuelGB1 <samuelgilbarceloiherm.amoros@gmail.com>
2021-10-12 22:11:10 +0200

    politica

---
+++ ppr/politica_privacidad.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="pprstyle.css">
+    <title>Preguntas Frecuentes</title>
+</head>
+<body>
+    <h1>Preguntas Frecuentes </h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nulla sodales aliquet pharetra. Duis vitae lorem tristique, 
+        volutpat eros id, mattis risus. Nullam ac tristique ipsum, 
+        in rhoncus augue. Sed non enim eget nisi porta ultrices. 
+        Duis tristique lobortis orci ac laoreet. Nam cursus libero nunc, 
+        sed porttitor felis venenatis maximus. Curabitur pulvinar aliquet euismod. 
+        Aenean pharetra laoreet viverra. Nam id magna et libero scelerisque gravida. 
+        Donec nec consectetur diam, nec pharetra sapien. Praesent vitae sapien lorem. 
+        Vestibulum mi nulla, auctor quis quam in, faucibus suscipit nisl. Ut bibendum, 
+        dui eu eleifend porttitor, augue sapien porttitor massa, eget mattis risus felis ut neque. 
+        Integer vel efficitur nunc. Sed mattis lobortis diam eget dapibus.</p>
+    <p>Ut finibus tortor lorem, sit amet faucibus diam vulputate vel. Sed eu ipsum ut turpis sollicitudin varius a vitae leo. 
+        Aliquam at nisl ac metus porta imperdiet. Suspendisse scelerisque mauris id ligula fermentum, ut vehicula eros lacinia. 
+        Phasellus in magna et felis facilisis aliquet eget eu lorem. Phasellus quis sapien et lacus maximus sagittis vel quis magna. 
+        Curabitur eget augue tortor. Maecenas feugiat nisi condimentum metus scelerisque, tincidunt blandit magna mollis. Morbi feugiat convallis diam, 
+        nec ullamcorper nibh molestie sed. Praesent imperdiet viverra lacinia. Integer sagittis varius ultrices. Nullam condimentum viverra tellus, 
+        nec porta lacus lacinia a. </p>
+    <p>Morbi condimentum eget ex rutrum dignissim. In tincidunt augue non condimentum pretium. Phasellus laoreet sollicitudin nibh, 
+        non fringilla eros porttitor quis. Vivamus eget maximus orci. In finibus et dui a porta. Integer vel quam sed felis rhoncus consectetur. 
+        Vestibulum placerat ut felis sit amet euismod. Curabitur id consectetur tortor. Pellentesque purus felis, placerat et lacus id, malesuada faucibus nunc. 
+        Pellentesque vitae massa eu enim elementum ornare vel vitae ex. Ut tincidunt blandit neque, non facilisis tellus vestibulum et. 
+        Suspendisse potenti. Morbi in rutrum metus. Nam lacinia leo consectetur tincidunt tristique. Donec consequat pellentesque elit a placerat. 
+        Mauris euismod consectetur nulla, sed convallis tortor imperdiet interdum. </p>
+    <p>Mauris at pellentesque diam. Nullam tempor urna vel sapien pulvinar gravida. Donec mauris risus, ultrices vel enim in, 
+        accumsan consequat lacus. Donec sed tincidunt nibh. Phasellus vitae justo mattis, iaculis turpis id, tristique quam. 
+        Nullam massa orci, auctor sit amet libero in, iaculis imperdiet tellus. Integer vestibulum sem eget suscipit tincidunt. 
+        Proin ut sem vitae sapien rutrum faucibus. Aenean vehicula eget purus ac tempor.</p>
+    <p>Donec ornare mauris ut nisl mattis semper. Vivamus condimentum condimentum mauris, a ullamcorper mi mattis id. Vestibulum id dolor congue, 
+        ullamcorper sapien eu, facilisis lectus. Integer ut augue hendrerit justo porta finibus et aliquam ex. 
+        Pellentesque dignissim, turpis imperdiet vulputate eleifend, quam tellus pellentesque nunc, quis sollicitudin sem libero id eros. 
+        In feugiat euismod nibh, eu convallis tortor sodales et. Maecenas nec leo quis lectus lobortis eleifend.</p>
+    <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed nisl sem, egestas a iaculis sit amet, 
+        pharetra hendrerit libero. Donec lectus justo, viverra quis congue blandit, viverra non ipsum. Sed fermentum vitae nulla ut posuere. 
+        Sed elementum tortor a ex molestie, a egestas urna blandit. Ut eget felis bibendum, pellentesque sem a, scelerisque nisi. Donec erat ligula, 
+        sollicitudin sit amet est eu, egestas cursus risus. Curabitur id consequat dui, id dignissim libero. In varius porttitor magna, vehicula sagittis tellus ultrices at. 
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed ante lorem, porttitor sed tortor nec, imperdiet lacinia tellus. 
+        Integer turpis lacus, vulputate non ipsum sed, convallis feugiat felis. Proin nec eros sed ex egestas faucibus in et mauris.</p>
+    <p>Cras vel dolor tortor. Nulla nec pharetra libero. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Phasellus aliquet consequat magna nec dignissim. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget est finibus, 
+        sagittis ipsum eget, pharetra sapien. Sed interdum ante in ex tempus, sagittis aliquam ex condimentum. Morbi tortor tellus, tincidunt efficitur commodo vel, 
+        pretium ac libero. In lobortis diam vitae venenatis bibendum. Ut sed lectus nulla. </p>
+    <p>Aliquam erat volutpat. Proin vehicula lectus non neque iaculis tincidunt. 
+        Quisque ultrices lorem vitae mi lacinia ultrices. Duis pellentesque nisi sed neque scelerisque, 
+        sit amet maximus ex sollicitudin. Fusce fringilla, felis ac hendrerit rutrum, metus eros luctus leo, nec pulvinar dui nisi a nisi. 
+        In molestie lectus quis pretium porta. Nunc suscipit sollicitudin massa, at semper magna lobortis et. In dolor lacus, lobortis in dignissim non, 
+        viverra a neque. Sed pretium fringilla volutpat. Quisque id purus mi. Nulla non augue non libero pharetra lobortis.</p>
+    <p>Nullam quis tincidunt libero. Phasellus luctus volutpat dui aliquam fringilla. Donec sollicitudin nunc at libero elementum faucibus. 
+        Phasellus in elementum dui, quis varius ligula. Etiam non lectus vel felis malesuada bibendum et sollicitudin lacus. Pellentesque nec 
+        ligula semper ante aliquet auctor id et ex. Nulla viverra nisi nulla, ut bibendum odio laoreet a. Etiam vitae convallis turpis, in euismod augue. 
+        Donec sed dolor at purus pellentesque molestie. Donec lobortis nulla quam, at vulputate augue facilisis ac. Sed ante massa, tempor sed semper vel, 
+        ultrices eget ante. Nunc congue nulla a ligula molestie, nec faucibus ex pellentesque.</p>
+    <?php
+        include 'menupiepag/menu.php';
+        include 'menupiepag/pie.php'
+    ?>
+</body>
+</html>

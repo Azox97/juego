607eb8dbcc7df5a68569489b1eb10add71fd04ba
Azox97 <73659420+Azox97@users.noreply.github.com>
2021-10-12 11:47:18 +0200

    retocado el mmenu

--- menu/menu.php
+++ menu/menu.php
@@ -10,7 +10,7 @@
             <ul>
                 <li><a href="http://localhost/juego/Index/index.php">Inicio</a></li>
                 <li><a href="http://localhost/juego/Index/preguntas_frecuentes.php">Preguntas Frecuentes </a></li>
-                <li><a href="http://localhost/juego/login/login.php">Preguntas Frecuentes </a></li>
+                <li><a href="http://localhost/juego/login/login.php">Cerrar sesion </a></li>
             </ul>    
         </nav>
     </body>
